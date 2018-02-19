389915382e5eb94a301a704830b67a371944b2b6
Donovan Clarke <dclarke@wagepoint.com>
2018-02-19 15:20:09 -0500

    update api

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,9 +1,30 @@
 // routes/note_routes.js
+var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
+  const data = db.db('noteapi');
+
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    data.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error': err})
+      } else {
+        res.send(item);
+      }
+    })
+  });
+
   app.post('/notes', (req, res) => {
     // create your note here.
-    console.log(req.body)
-    res.send('Hello')
+    const note = { text: req.body.body, title: req.body.title };
+    data.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': err});
+      } else {
+        res.send(note)
+      }
+    })
   });
 };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
 
@@ -12,8 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
-
-  require('./app/routes')(app, {});
+  // console.log(database);
+  require('./app/routes')(app, database);
   app.listen(port, () => {
     console.log('we are live on ' + port);
   })
